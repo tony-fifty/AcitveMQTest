d394ddea31bdf3b838f763cb6b7402b7eb80d929
huangC <156452036@qq.com>
2018-12-05 23:58:06 +0800

    2018年12月5日

--- src/main/java/com/hc/consumer/JMSConsumer.java
+++ src/main/java/com/hc/consumer/JMSConsumer.java
@@ -12,16 +12,23 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 
 public class JMSConsumer {
 	public static void main(String[] args) throws JMSException {
+		// 创建连接池
 		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://127.0.0.1:61616");
+		// 创建连接
 		Connection connection = connectionFactory.createConnection();
+		// 开启连接
 		connection.start();
+		// 创建会话
 		Session session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
-		Destination destination = session.createQueue("hc");
+		// 创建目的地
+		Destination destination = session.createQueue("hc1");
+		// 创建消费者
 		MessageConsumer messageConsumer = session.createConsumer(destination);
-		for (int i = 0; i < 3; i++) {
-			TextMessage message = (TextMessage) messageConsumer.receive();
-			System.out.println(message.getText());
+		int i = 0;
+		while (i < 4) {
+			TextMessage textMessage = (TextMessage) messageConsumer.receive();
 			session.commit();
+			System.out.println(textMessage.getText());
 		}
 		session.close();
 		connection.close();
--- src/main/java/com/hc/producer/JMSProducer.java
+++ src/main/java/com/hc/producer/JMSProducer.java
@@ -12,15 +12,25 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 
 public class JMSProducer {
 	public static void main(String[] args) throws JMSException {
+		// 创建连接工厂
 		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://127.0.0.1:61616");
+		// 创建连接
 		Connection connection = connectionFactory.createConnection();
+		// 发送端可以不需要,接收端必须启动连接
 		connection.start();
+		// 创建回会话
 		Session session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
-		Destination destination = session.createQueue("hc");
-		MessageProducer messageProducer = session.createProducer(destination);
-		for (int i = 0; i < 3; i++) {
-			TextMessage message = session.createTextMessage("hc--" + i);
-			messageProducer.send(message);
+		// 创建目的地
+		Destination destination = session.createQueue("hc1");
+		// 创建生产者
+		MessageProducer message = session.createProducer(destination);
+		for (int i = 0; i < 4; i++) {
+			// 发送消息
+			// TextMessage textMessage = session.createTextMessage("发送" + i);
+			TextMessage textMessage = session.createTextMessage();
+			textMessage.setText("hc1发送" + i);
+			// 通过消息生产者发出消息
+			message.send(textMessage);
 		}
 		session.commit();
 		session.close();
